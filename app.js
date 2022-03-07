d02ff9275ae1a7307560db6ad7f5096cd8cdd1e8
Gene Wong <gene.h.wong@gmail.com>
2022-03-07 12:11:04 -0800

    implemented state

--- app.js
+++ app.js
@@ -1,1 +1,45 @@
-// TODO
+var App = () => (
+  <div>
+    <h2>My Todo List</h2>
+    <GroceryList groceries={['Kale', 'Bread', 'Oranges']} />
+  </div>
+)
+
+var GroceryList = (props) => (
+  <ul>
+    {props.groceries.map(item => <GroceryListItem grocery={item} />
+    )}
+  </ul>
+)
+
+class GroceryListItem extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      hover: false
+    }
+  }
+
+  onHover() {
+    this.setState({
+      hover: !this.state.hover
+    });
+  }
+
+  render() {
+
+    var style = {
+      fontWeight: this.state.hover ? 'bold' : 'normal'
+    };
+
+    return (
+      <li style={style} onMouseOver={this.onHover.bind(this)} onMouseOut={this.onHover.bind(this)}>
+        {this.props.grocery}
+      </li>
+    );
+  }
+}
+
+
+ReactDOM.render(<App />, document.getElementById("app"));
